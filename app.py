7923a9cf11375f3b1b720813f4e71f2e2fe764ab
Aishwarya-rav248 <aishwaryaravinderraj@gmail.com>
2025-04-02 23:36:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ def show_login_page():
     st.title("Welcome to HealthPredict")
     st.subheader("Login with Patient ID")
 
-    patient_id = st.text_input("Enter Patient ID")
-    st.write("Columns in uploaded CSV:", df.columns.tolist())
-
+    patient_id = st.text_input("Enter Patient ID") 
 
     if st.button("Login"):
         data_path = "selected_20_final_patients.csv"
@@ -25,7 +23,8 @@ def show_login_page():
             st.error(f"Failed to read data file. Error: {str(e)}")
             return
 
-        if patient_id in df["patient_id"].values:
+        
+        if patient_id in df["patient"].astype(str).values:
             st.session_state.logged_in = True
             st.session_state.patient_id = patient_id
             st.experimental_rerun()
