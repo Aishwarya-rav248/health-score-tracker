61befccd89e8d8d228c8fc7d8ffbe7ec85ae4251
Aishwarya-rav248 <aishwaryaravinderraj@gmail.com>
2025-04-02 23:08:26 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+from patient_login import show_login_page
+from patient_dashboard import show_dashboard_page
+
+# Initialize session state variables
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+    st.session_state.patient_id = ""
+
+# App Navigation
+if st.session_state.logged_in:
+    show_dashboard_page(st.session_state.patient_id)
+else:
+    show_login_page()
